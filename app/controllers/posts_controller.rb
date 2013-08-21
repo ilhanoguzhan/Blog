197120c88bfddbc4c2976e115774eb6bb1e1bb10
OCC <occ@occ-R560.(none)>
2013-08-21 14:21:09 +0300

    Post controller user auth. added.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  http_basic_authenticate_with name: "occ", password: "pass", except: [:index, :show]
+
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
